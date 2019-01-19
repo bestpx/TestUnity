ca7399f0bc814f544bac57df0e679411bf0602c3
TofuBrain <bestpx@gmail.com>
2019-01-19 05:34:46 -0800

    remove dup project

--- TestAssetBundleDup/Assets/Scripts/LevelAssets.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[CreateAssetMenu(fileName = "LevelAssets", menuName = "LevelAssets", order = 1)]
-public class LevelAssets : ScriptableObject {
-    public Sprite Background;
-    public Sprite Button;
-    public Sprite Title;
-}
