884e75c275e41536b89a9987fe022e56eac36d9b
Dennis Pan <dennis@scopely.com>
2019-01-20 18:19:42 -0800

    2017.4.17f1 test

---
+++ TestAssetBundle2017.4.17f1/Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class AssetBundleManager : MonoBehaviour
+{
+    public static AssetBundleManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public AssetBundle LoadAssetBundle()
+    {
+        var abPath = Path.Combine(Application.dataPath, "AssetBundles");
+        var filePath = Path.Combine(abPath, "Level1");
+        var ab = AssetBundle.LoadFromFile(filePath);
+        return ab;
+    }
+}
---
+++ TestAssetBundle2017.4.17f1/Assets/Scripts/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class AssetBundleBuilder : MonoBehaviour
+{
+    [MenuItem("Tools/BuildAssetBundle")]
+    public static void BuildAssetBundle()
+    {
+        var outputPath = Path.Combine(Application.dataPath, "AssetBundles");
+        BuildPipeline.BuildAssetBundles(outputPath, 
+            UnityEditor.BuildAssetBundleOptions.ForceRebuildAssetBundle, EditorUserBuildSettings.activeBuildTarget);
+    }
+}
