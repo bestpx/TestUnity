047365c61cbb12e01773c45a98493ca848d933dd
Dennis Pan <dennis@scopely.com>
2019-01-20 17:40:54 -0800

    2017.4.1f1 version

---
+++ TestAssetBundle2017.4.1f1/Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class AssetBundleManager : MonoBehaviour
+{
+    public static AssetBundleManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public AssetBundle LoadAssetBundle()
+    {
+        var abFolderPath = Path.Combine(Application.dataPath, "AssetBundles");
+        var filePath = Path.Combine(abFolderPath, "level1");
+        var ab = AssetBundle.LoadFromFile(filePath);
+        return ab;
+    }
+}
---
+++ TestAssetBundle2017.4.1f1/Assets/Scripts/UIView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIView : MonoBehaviour
+{
+    public Image Background;
+    public Image Title;
+    public Image Button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var ab = AssetBundleManager.Instance.LoadAssetBundle();
+        Background.sprite = ab.LoadAsset<Sprite>("background");
+        Title.sprite = ab.LoadAsset<Sprite>("title");
+        Button.sprite = ab.LoadAsset<Sprite>("button");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
